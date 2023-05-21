e3428bd61f045275b6a0e95e10326931af29b2c5
yuldashevmuhiddin65 <102411263+yuldashevmuhiddin65@users.noreply.github.com>
2023-05-21 15:48:19 +0500

    Fixed bugs

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int x = Math.round(numberToBeRounded);
+        int num = (int) x;
+
+        System.out.println(num);
+
     }
 }
--- src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
+++ src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
@@ -4,12 +4,12 @@ public class OperatorPrecedence {
     public static void main(String[] args) {
         int first = 1;
         int second = 2;
-        int eight = 3 + first * second;
+        int eight = (3 + first) * second;
         System.out.println(eight);//eight is expected to be 8
         boolean firstBool = false;
         boolean secondBool = false;
-        boolean thirdBool = 1 == 1;
-        boolean fourthBool = 4 <= 101;
+        boolean thirdBool = false;
+        boolean fourthBool = false;
         int varSix = 17;
 
         System.out.println(firstBool && secondBool || thirdBool && fourthBool || (varSix = 5) < 0);
